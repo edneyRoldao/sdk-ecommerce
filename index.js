3f9e4c34391db162d4fa99e3cb700e8256e53a39
edneyRoldao <edneyroldao@gmail.com>
2021-05-05 12:21:54 -0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 const expressServer = require('./config/express.config');
 
-const serverPort = process.env.port || 9010;
+const serverPort = process.env.PORT || 9010;
 
 expressServer().listen(serverPort, () => {
     console.log('ecommerce-sdk has been started');    

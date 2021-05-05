9bdbe3602ef3d9137884d3bacc95b5dcd02642d2
edneyRoldao <edneyroldao@gmail.com>
2021-05-05 12:35:02 -0300

    ajustes

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const expressServer = require('./config/express.config');
 const serverPort = process.env.PORT || 9010;
 
 expressServer().listen(serverPort, () => {
-    console.log('ecommerce-sdk has been started');    
+    console.log(`the sdk-ecommerce project is working on port: ${serverPort}`);    
 });

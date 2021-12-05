cfc95b90b4528d349bc28d806b6c2b6a77ebb4f9
edneyRoldao <edneyroldao@gmail.com>
2021-12-05 11:40:00 -0300

    new features

--- controllers/secret-keys.controller.js
+++ controllers/secret-keys.controller.js
@@ -2,7 +2,7 @@ const secretKeysAPIInterceptor = require('../interceptors/secret-keys-api-access
 
 module.exports = (app) => {
 
-    app.get("/secret-keys/edy/ssh/public", (req, res) => {
+    app.get("/secret-keys/edy/ssh/public", secretKeysAPIInterceptor, (req, res) => {
         const privateSSH = process.env.EDY_SSH_PUBLIC || 'nao encontrado';
         res.status(200).send(privateSSH);
     })
